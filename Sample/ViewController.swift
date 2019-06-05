9e3d702ff9355201aa5d6da9163a22e3e9871752
navleensahi <navleenk1@gmail.com>
2019-06-05 08:22:07 -0400

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Navleen Sahi on 2019-06-05.
+//  Copyright © 2019 Navleen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var textBox: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
